dedbed6bfbb28349f2d5d80c41bba158859961bf
Tony Duong <tony.duong.102@gmail.com>
2025-03-30 15:48:27 +0900

    :sparkles: add application with devise authentication

---
+++ config/initializers/cors.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Avoid CORS issues when API is called from the frontend app.
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
+
+# Read more: https://github.com/cyu/rack-cors
+
+# Configure CORS to accept cross-domain requests from the frontend app.
+# Reference: https://devise-token-auth.gitbook.io/devise-token-auth/config/cors
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "localhost:3000"
+
+    resource "*",
+      headers: :any,
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
